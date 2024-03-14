e1f0f5b0ee1c7e8a2333db7d51a06fd520721e58
Sayan Bose <sayan.bose.lmg.2000@gmail.com>
2024-03-14 10:44:43 +0530

    Update

--- src/agent.py
+++ src/agent.py
@@ -71,7 +71,7 @@ class Agent:
         self.env.reset()
         rewards_per_trial = []
         for trials in tqdm(range(300)):
-            grad = np.zeros(np.size(self.theta))
+            # grad = np.zeros(np.size(self.theta))
             rewards = [self.run_episode()]
             max_reward = rewards[0]
             for episode in range(100):
@@ -111,4 +111,71 @@ class Agent:
         plt.xlabel("Episodes")
         plt.ylabel("Mean rewards")
         plt.title("Mean rewards per trial with error bars")
+        plt.show()
+
+    def cross_entropy(self, eps):
+        """
+        This function is used to learn the optimal policy using the Cross-Entropy method.
+        """
+        self.env.reset()
+        # Fix K, Keps
+        K = 100
+        Keps = int(K * eps)
+        # Initialize theta
+        theta = np.zeros(5)
+        self.set_theta(theta)
+        max_reward_so_far = np.mean([self.run_episode() for _ in range(10)])
+        # Initialize covariance matrix
+        sigma = 2 * np.eye(5)
+        rewards_per_episode = [[] for i in range(10)]
+        for it in tqdm(range(300)):
+            rewards_per_theta = []
+            for k in range(1, K + 1):
+                new_theta = np.random.multivariate_normal(self.theta, sigma)
+                cur_theta = self.theta
+                self.set_theta(new_theta)
+                rewards = []
+                for episode_count in range(10):
+                    rewards.append(self.run_episode())
+                    rewards_per_episode[episode_count].append(rewards[-1])
+                rewards_per_theta.append([np.mean(rewards), new_theta])
+                if np.mean(rewards) > max_reward_so_far:
+                    max_reward_so_far = np.mean(rewards)
+                else:
+                    self.set_theta(cur_theta)
+            rewards_per_theta.sort(key=lambda x: x[0], reverse=True)
+            # Pick Keps best thetas
+            best_thetas = [theta for _, theta in rewards_per_theta][:Keps]
+            # Find the mean and covariance of the best thetas
+            mean_theta = np.mean([x[1] for x in best_thetas], axis=0)
+            temp = np.sum(
+                [
+                    np.multiply(
+                        np.subtract(theta, mean_theta),
+                        np.transpose(np.subtract(theta, mean_theta)),
+                    )
+                    for theta in best_thetas
+                ]
+            )
+            sigma = (1.0 / (eps + Keps)) * (eps * 2 * np.eye(5) + temp)
+        mean_reward_per_episode = [np.mean(x) for x in rewards_per_episode]
+        # PLot the mean rewards per episode
+        plt.figure(figsize=(20, 6))
+        plt.plot(mean_reward_per_episode)
+        plt.xlabel("Episodes")
+        plt.ylabel("Mean rewards")
+        plt.title("Mean rewards per episode")
+        plt.show()
+        # Plot the error bars
+        std_dev_per_episode = [np.std(x) for x in rewards_per_episode]
+        plt.figure(figsize=(20, 6))
+        plt.errorbar(
+            range(len(mean_reward_per_episode)),
+            mean_reward_per_episode,
+            yerr=std_dev_per_episode,
+            fmt="o",
+        )
+        plt.xlabel("Episodes")
+        plt.ylabel("Mean rewards")
+        plt.title("Mean rewards per episode with error bars")
         plt.show()
